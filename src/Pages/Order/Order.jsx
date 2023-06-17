1bc881597982785e1209c27baa39711dbbb38cf1
kasraMg <kasragithub7@gmail.com>
2023-06-17 13:19:15 +0330

    orderApi

--- src/Pages/Order/Order.jsx
+++ src/Pages/Order/Order.jsx
@@ -1,25 +1,26 @@
 import React, { useEffect, useState, useRef } from 'react'
 import './Order.css'
 import Header from '../../Components/Header/Header'
-import Footer from '../../Components/Footer/Footer' 
-import { useNavigate, useParams } from 'react-router-dom' 
+import Footer from '../../Components/Footer/Footer'
+import { useNavigate, useParams } from 'react-router-dom'
 import swal from 'sweetalert'
 import Loader from '../../Components/Loader/Loader'
 import FinalOrderStatus from '../../Components/FinalOrderStatus/FinalOrderStatus'
 import FirstOrderStatus from '../../Components/FirstOrderStatus/FirstOrderStatus'
+import OrderDetail from '../../Components/OrderDetail/OrderDetail'
 export default function Order() {
   const [details, setDetails] = useState()
   const [Minutes, setMinutes] = useState()
   const [seconds, setSeconds] = useState()
   const [Count, setCount] = useState();
-  const [waiting, setWaiting] = useState()
+
   const [status200, setstatus200] = useState();
-  const [finalStatus, setFinalStatus] = useState();
+
   const [Status404, setStatus404] = useState();
   const [LoaderStatus, setLoader] = useState(true);
   let hashChange = useParams()
   const navigate = useNavigate()
-  
+
   useEffect(() => {
     if (hashChange) {
       setInterval(() => {
@@ -30,10 +31,10 @@ export default function Order() {
             setDetails(data)
             if (data.status == 200) {
               setstatus200(true)
-              setWaiting(false)
-              setFinalStatus(false)
+
+
             }
-            else if (data.status == 404) {
+            else if (data.status == 404 || data.code == 404) {
               setStatus404(true)
               swal({
                 title: 'Submit a New Request',
@@ -46,15 +47,15 @@ export default function Order() {
 
 
             } else if (data.status == 201) {
-              setFinalStatus(true)
+
               setstatus200(false)
-              setWaiting(false)
+
             } else if (data.status == 300) {
-              setWaiting(true)
+
               setstatus200(false)
-              setFinalStatus(false)
+
             }
-        
+
             setLoader(false)
             console.log(data)
             if (data.secondsleft > 1801) {
@@ -87,8 +88,8 @@ export default function Order() {
             if (data.status == 200) {
               setstatus200(true)
             }
-            else if (data.status == 404) {
-
+            else if (data.status == 404 || data.code == 404) {
+              setStatus404(true)
               swal({
                 title: 'Submit a New Request',
                 icon: 'error',
@@ -102,7 +103,7 @@ export default function Order() {
             }
             setDetails(data)
             setLoader(false)
-            console.log(data)
+            console.log(data.code)
             if (data.secondsleft > 1801) {
               setMinutes(`0`)
               setSeconds(`0000`)
@@ -134,15 +135,20 @@ export default function Order() {
             <>
               <img src="/images/order/icons8-wait-96.png" className='wait-img' alt="" />
               <p className='please-wait-title'>Please Wait...</p>
+              <div className='order-details'>
+                <OrderDetail icon='order-await' text='Awaiting deposit' flash={true} dark={false} />
+                <OrderDetail icon='order-wait' text='Awaiting confirmations' flash={true} dark={false} />
+                <OrderDetail icon='order-done' text='Done' flash={false} dark={true} />
+              </div>
             </>
           )}
 
-          {waiting && (
-           <FirstOrderStatus  Minutes={Minutes}deatils={details}/>
+          {details.status == 300 && (
+            <FirstOrderStatus Minutes={Minutes} details={details} />
           )}
 
-          {finalStatus && (
-          <FinalOrderStatus details={details}/>
+          {details.status == 201 && (
+            <FinalOrderStatus details={details} />
           )}
 
           {Status404 && (
